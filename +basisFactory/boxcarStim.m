71c733b2200c60c59c17a35efc9cae0573b9a042
Jacob Yates <jacobyates@la-cps884733md-huk.psy.utexas.edu>
2015-10-30 14:24:39 -0500

    throw out times that occur outside the trial

--- +basisFactory/boxcarStim.m
+++ +basisFactory/boxcarStim.m
@@ -1,6 +1,8 @@
 function stim = boxcarStim(startBinnedTime, endBinnedTime, nT)
 % Returns a boxcar duration stimulus design
-
+if endBinnedTime>nT
+    endBinnedTime=nT;
+end
 idx = startBinnedTime:endBinnedTime;
 o = ones(numel(idx), 1);
 
